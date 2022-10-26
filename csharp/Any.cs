2769c02bb8a4c55653826cd14c4a46c12f316060
cdgraybill <cdgraybill@gmail.com>
2022-10-26 00:17:15 -0500

    created tests

---
+++ csharp/Any.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    internal class Any
+    {
+        private static readonly Random SellIn = new Random();
+        private static Random Quality = new Random();
+
+        public List<Item> GenerateGenericItem()
+        {
+            Item item = GenerateItemTemplate("Sword");
+
+            return new List<Item> { item };
+        }
+
+        public List<Item> GenerateAgedBrie()
+        {
+            Item item = GenerateItemTemplate("Aged Brie");
+
+            return new List<Item> { item };
+        }
+
+        public List<Item> GenerateSulfuras()
+        {
+            Item item = GenerateItemTemplate("Sulfuras, Hand of Ragnaros");
+
+            return new List<Item> { item };
+        }
+
+        public List<Item> GenerateBackstagePass(int minDaysLeft = 12, int maxDaysLeft = 30)
+        {
+            Item item = GenerateItemTemplate("Backstage passes to a TAFKAL80ETC concert");
+            item.SellIn = SellIn.Next(minDaysLeft, maxDaysLeft);
+
+            return new List<Item> { item };
+        }
+
+        private static Item GenerateItemTemplate(string itemName)
+        {
+            var item = new Item()
+            {
+                Name = itemName,
+                Quality = Quality.Next(2, 49),
+                SellIn = SellIn.Next(2, 49)
+            };
+
+            return item;
+        }
+    }
+}
--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 
 namespace csharp
@@ -7,12 +8,153 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void UpdateItems_LowerSellInByOne_WhenGenericItem()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            var any = new Any();
+
+            var items = any.GenerateGenericItem();
+            var sellIn = items[0].SellIn;
+
+            GildedRose app = new GildedRose(items);
+            app.UpdateItems();
+
+            Assert.AreEqual("Sword", items[0].Name);
+            Assert.AreEqual(sellIn - 1, items[0].SellIn);
+        }
+
+        [Test]
+        public void UpdateItems_LowerQualityByOne_WhenGenericItem()
+        {
+            var any = new Any();
+
+            var items = any.GenerateGenericItem();
+            var quality = items[0].Quality;
+
+            GildedRose app = new GildedRose(items);
+            app.UpdateItems();
+
+            Assert.AreEqual("Sword", items[0].Name);
+            Assert.AreEqual(quality - 1, items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateItems_LowerSellInByOne_WhenAgedBrie()
+        {
+            var any = new Any();
+
+            var items = any.GenerateAgedBrie();
+            var sellIn = items[0].SellIn;
+
+            GildedRose app = new GildedRose(items);
+            app.UpdateItems();
+
+            Assert.AreEqual("Aged Brie", items[0].Name);
+            Assert.AreEqual(sellIn - 1, items[0].SellIn);
+        }
+
+        [Test]
+        public void UpdateItems_IncreaseQualityByOne_WhenAgedBrie()
+        {
+            var any = new Any();
+
+            var items = any.GenerateAgedBrie();
+            var quality = items[0].Quality;
+
+            GildedRose app = new GildedRose(items);
+            app.UpdateItems();
+
+            Assert.AreEqual("Aged Brie", items[0].Name);
+            Assert.AreEqual(quality + 1, items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateItems_SellInDoesNotChange_WhenSulfuras()
+        {
+            var any = new Any();
+
+            var items = any.GenerateSulfuras();
+            var sellIn = items[0].SellIn;
+
+            GildedRose app = new GildedRose(items);
+            app.UpdateItems();
+
+            Assert.AreEqual("Sulfuras, Hand of Ragnaros", items[0].Name);
+            Assert.AreEqual(sellIn, items[0].SellIn);
+        }
+
+        [Test]
+        public void UpdateItems_QualityDoesNotChange_WhenSulfuras()
+        {
+            var any = new Any();
+
+            var items = any.GenerateSulfuras();
+            var quality = items[0].Quality;
+
+            GildedRose app = new GildedRose(items);
+            app.UpdateItems();
+
+            Assert.AreEqual("Sulfuras, Hand of Ragnaros", items[0].Name);
+            Assert.AreEqual(quality, items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateItems_QualityIncreasesByOne_WhenBackstagePass()
+        {
+            var any = new Any();
+
+            var items = any.GenerateBackstagePass();
+            var quality = items[0].Quality;
+
+            GildedRose app = new GildedRose(items);
+            app.UpdateItems();
+
+            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", items[0].Name);
+            Assert.AreEqual(quality + 1, items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateItems_QualityIncreasesByTwo_WhenBackstagePass_TenToSixDaysLeft()
+        {
+            var any = new Any();
+
+            var items = any.GenerateBackstagePass(6, 11);
+            var quality = items[0].Quality;
+
+            GildedRose app = new GildedRose(items);
+            app.UpdateItems();
+
+            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", items[0].Name);
+            Assert.AreEqual(quality + 2, items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateItems_QualityIncreasesByThree_WhenBackstagePass_FiveToTwoDaysLeft()
+        {
+            var any = new Any();
+
+            var items = any.GenerateBackstagePass(2, 5);
+            var quality = items[0].Quality;
+
+            GildedRose app = new GildedRose(items);
+            app.UpdateItems();
+
+            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", items[0].Name);
+            Assert.AreEqual(quality + 3, items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateItems_QualityIsZero_WhenBackstagePass_AfterConcert()
+        {
+            var any = new Any();
+
+            var items = any.GenerateBackstagePass(0, 1);
+            var quality = items[0].Quality;
+
+            GildedRose app = new GildedRose(items);
+            app.UpdateItems();
+
+            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", items[0].Name);
+            Assert.AreEqual(quality - quality, items[0].Quality);
         }
     }
 }
